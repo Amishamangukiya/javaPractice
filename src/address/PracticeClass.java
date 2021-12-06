e65de54705db7cb32f4200e0b3a6db94a5056240
Amishamangukiya <amishamangukiya@gmail.com>
2021-12-06 17:13:24 +0000

    practice class

---
+++ src/address/PracticeClass.java
@@ -0,0 +1,8 @@
+package address;
+
+public class PracticeClass {
+    public static void main(String[] args) {
+        System.out.println("amisha");
+        System.out.println("mangukiya");
+    }
+}
